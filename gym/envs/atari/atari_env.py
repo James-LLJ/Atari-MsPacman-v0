8403512ab9ca61e0b96dec17860d58f45082737c
James-LLJ <42400077+James-LLJ@users.noreply.github.com>
2021-04-07 14:43:44 +0800

    Update atari_env.py

--- gym/envs/atari/atari_env.py
+++ gym/envs/atari/atari_env.py
@@ -4,6 +4,10 @@ import gym
 from gym import error, spaces
 from gym import utils
 from gym.utils import seeding
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+import sys
 
 try:
     import atari_py
@@ -19,17 +23,61 @@ def to_ram(ale):
     ale.getRAM(ram)
     return ram
 
+#global variable to store the previous frames RGB
+prevRGB=0
+
+#function to find coordinates of Aliens and Agent
+def coordinates(diffmat,bval):
+  wherer = np.equal(diffmat[:,:,2], bval)
+
+  indices = np.where(wherer) 
+
+  listx=[]
+  listy=[]
+  for i in range(0,len(indices[0])):
+    tempy= indices[0][i]
+    tempx= indices[1][i]
+
+    for j in range(0,len(indices[0])):
+
+      if tempy == indices[0][j] and abs(tempx - indices[1][j]) == 1:
+        listx=np.append(listx, tempx)
+        listy=np.append(listy, tempy)
+
+  return np.average(listx), np.average(listy)
+ 
+#function to locate position of eggs 
+def cooregg(d1) :
+  wherer = np.not_equal(d1[:,:,2], 0)
+  indices = np.where(wherer) 
+
+  listx=[]
+  listy=[]
+  
+  for i in range(0,len(indices[0])):
+    tempy= indices[0][i]
+    tempx= indices[1][i]
+    if tempy < 169:
+      if d1[tempy+1,tempx,2] != 0  and  d1[tempy-1,tempx,2] == 0 and  d1[tempy,tempx-1,2] == 0 and  d1[tempy,tempx+1,2] == 0 and  d1[tempy-1,tempx-1,2]==0 and  d1[tempy-1,tempx+1,2] == 0 and  d1[tempy+1,tempx-1,2] == 0 and  d1[tempy+1,tempx+1,2] == 0 and  d1[tempy+2,tempx,2] != 188 and  d1[tempy+3,tempx,2] != 188 and  d1[tempy+2,tempx,2] != 28 and  d1[tempy+3,tempx,2] != 28:
+        listx=np.append(listx, tempx)
+        listy=np.append(listy, tempy) 
+  return listx, listy
+
+#function to determine distance between objects using pythagoras
+def distance(x1,x2,y1,y2):
+  distance= math.sqrt((x1-x2)**2+(y1-y2)**2)
+  return distance
 
 class AtariEnv(gym.Env, utils.EzPickle):
     metadata = {'render.modes': ['human', 'rgb_array']}
-
+    
     def __init__(
             self,
             game='pong',
             mode=None,
             difficulty=None,
             obs_type='ram',
-            frameskip=(2, 5),
+            frameskip=5,
             repeat_action_probability=0.,
             full_action_space=False):
         """Frameskip should be either a tuple (indicating a random range to
@@ -108,19 +156,86 @@ class AtariEnv(gym.Env, utils.EzPickle):
 
         return [seed1, seed2]
 
-    def step(self, a):
+    def step( self, a ):
         reward = 0.0
+
+        #creating new variable to store value of original reward function
+        gamescore=0.0
+       
+        global prevRGB
         action = self._action_set[a]
 
         if isinstance(self.frameskip, int):
             num_steps = self.frameskip
         else:
             num_steps = self.np_random.randint(self.frameskip[0], self.frameskip[1])
+        
         for _ in range(num_steps):
-            reward += self.ale.act(action)
+            gamescore += self.ale.act(action)
+
         ob = self._get_obs()
 
-        return ob, reward, self.ale.game_over(), {"ale.lives": self.ale.lives()}
+        
+        #Finding the difference in RGB for the very first frame and the current frame
+
+        x= self.x[0:175,:]
+
+        y=ob[0:175,:]
+        d= x-y
+
+        #Determining distance between agent and aliens
+
+        [xcoragent,ycoragent]= coordinates(d,188)
+        [xcorAlienA,ycorAlienA]= coordinates(d,216)
+       
+        distA= distance(xcoragent,xcorAlienA,ycoragent,ycorAlienA)
+        
+        [xcorAlienB,ycorAlienB]= coordinates(d,100)
+        
+        distB= distance(xcoragent,xcorAlienB,ycoragent,ycorAlienB)
+        
+        [xcorAlienC,ycorAlienC]= coordinates(d,228)
+        distC= distance(xcoragent,xcorAlienC,ycoragent,ycorAlienC)
+        
+        
+        #Finding difference between previous RGB frame and current frame
+        d1= prevRGB - y
+        prevRGB= y
+
+        #Finding location of each egg and determining which is closest to agent
+        [xcorEgg,ycorEgg]= cooregg(d1)
+
+        #print(len(xcorEgg))
+
+        mindist = 1000
+        for i in range(0,len(xcorEgg)):
+          tempdist= math.sqrt((xcoragent-xcorEgg[i])**2+(ycoragent-ycorEgg[i])**2)
+          if tempdist < mindist :
+            mindist=tempdist
+
+        #print('Nearest Egg:',mindist)
+
+        
+        #determining the distance of the closest alien
+
+        minAlien= min(distA,distB,distC)
+
+        if math.isnan(distA) and math.isnan(distB) and math.isnan(distC):
+          minAlien=1000
+
+        #print('Nearest Alien:',minAlien)
+
+        #Setting up conditions for new reward function
+        
+        if mindist < 20 and minAlien > 40  :
+          reward=reward + 10
+
+        if minAlien <= 40:
+          reward= reward - 10
+
+        #print('reward:', reward)
+
+        return minAlien, mindist, ob, gamescore, reward, self.ale.game_over(), {"ale.lives": self.ale.lives()}
 
     def _get_image(self):
         return self.ale.getScreenRGB2()
@@ -141,7 +256,12 @@ class AtariEnv(gym.Env, utils.EzPickle):
 
     # return: (states, observations)
     def reset(self):
+
         self.ale.reset_game()
+        self.x = self._get_obs()
+        global prevRGB
+        prevRGB= self._get_obs()[0:175,:]
+        
         return self._get_obs()
 
     def render(self, mode='human'):
