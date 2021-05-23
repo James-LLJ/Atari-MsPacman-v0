6f5a945db61f9d1191eb43bc2f96a23706a0634d
James-LLJ <42400077+James-LLJ@users.noreply.github.com>
2021-05-23 19:22:10 +0800

    Update atari_env.py

--- gym/envs/atari/atari_env.py
+++ gym/envs/atari/atari_env.py
@@ -58,8 +58,7 @@ def cooregg(d1) :
     tempy= indices[0][i]
     tempx= indices[1][i]
     if tempx < 150 and tempy<168:
-      #if d1[tempy+1,tempx,2] == 111 and d1[tempy+2,tempx,2] == 136  and  d1[tempy,tempx+4,2] == 136 and  d1[tempy,tempx+3,2] == 111 and  d1[tempy,tempx+2,2] == 111 and  d1[tempy,tempx+1,2] == 111 and d1[tempy,tempx-1,2] == 136 and d1[tempy-1,tempx,2] == 136 : 
-      if d1[tempy+1,tempx,2] == 111 and d1[tempy+2,tempx,2] == 136  and  d1[tempy,tempx+4,2] == 136 :
+      if d1[tempy+1,tempx,2] == 111 and d1[tempy+2,tempx,2] == 136  and  d1[tempy,tempx+4,2] == 136 and  d1[tempy,tempx+3,2] == 111 and  d1[tempy,tempx+2,2] == 111 and  d1[tempy,tempx+1,2] == 111 and d1[tempy,tempx-1,2] == 136 and d1[tempy-1,tempx,2] == 136 : 
         listx=np.append(listx, tempx)
         listy=np.append(listy, tempy) 
   listx = [x+2 for x in listx]
